9e6a242e1281514a6bf0572ae95c2fc88bfd713a
nickc1 <nick.cortale@gmail.com>
2019-01-13 12:15:05 -0500

    working api

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi:python3.7
 
+RUN pip install joblib scikit-learn
+
+COPY ./model /model
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI
+from joblib import load
+from pydantic import  BaseModel
+
+
+# define model for post request. Not needed if just implementing get
+class ModelParams(BaseModel):
+    param1: float
+    param2: float
+
 
 app = FastAPI()
+
+clf = load('/model/model_1.joblib')
+
+def get_prediction(param1, param2):
+    
+    x = [[param1, param2]]
+
+    y = clf.predict(x)[0]  # just get single value
+    prob = clf.predict_proba(x)[0].tolist()  # send to list for return
+
+    return {'prediction': int(y), 'probability': prob}
 
 
 @app.get("/")
@@ -8,6 +28,20 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/predict/{param1}/{param2}")
+def predict(param1: float, param2: float):
+
+    pred = get_prediction(param1, param2)
+
+    return pred
+
+
+@app.post("/predict-post/")
+def post_predict(params: ModelParams):
+
+    # param_dict = params.dict()
+    # print(param_dict)
+    pred = get_prediction(params.param1, params.param2)
+
+    return pred
+
